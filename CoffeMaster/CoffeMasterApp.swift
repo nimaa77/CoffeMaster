f3650249fc71b50faffe753612814c2750e7cdc5
Nima Arefi <nimaarefi@outlook.com>
2022-06-25 17:05:17 +0430

    Initial Commit

---
+++ CoffeMaster/CoffeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeMasterApp.swift
+//  CoffeMaster
+//
+//  Created by Nima Arefi on 4/4/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct CoffeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeMaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoffeMaster
+//
+//  Created by Nima Arefi on 4/4/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
